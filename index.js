f8580162ec5f8f968372e8b3fd995a281dacb51e
Dan <danpopa1998@gmail.com>
2017-07-24 19:08:00 +0300

    get paths from images

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const PLUGIN_NAME = 'gulp-image-to-imgur';
-var through = require('through2'),
-    gutil = require('gulp-util'),
-    PluginError = gutil.PluginError;
+const through = require('through2');
+const gutil = require('gulp-util');
+const PluginError = gutil.PluginError;
+const DOMParser = require('xmldom').DOMParser;
 /**
  * This method transform the input string to upper/lower case
  * @param caseType - The transform type upper/lower
  * @param inputString - The input string
  * @returns {string} - The transformed string
  */
-var transformText = function(caseType, inputString) {
-    var outString = null;
-    switch (caseType) {
-        case 'uppercase':
-            outString = inputString.toUpperCase();
-            break;
-        case 'lowercase':
-            outString = inputString.toLowerCase();
-            break;
-        default:
-            outString = inputString;
-            break;
-    }
-    return outString;
+function transformText(input) {
+    const paths = getPaths(input)
+    return input;
 };
+
+function getPaths(input) {
+    let paths = [];
+    const html = new DOMParser().parseFromString(input, 'text/html');
+    const images = html.getElementsByTagName('img');
+    Array.prototype.forEach.call(images, image => {
+        paths.push(image.getAttribute('src'));
+    });
+    console.log(paths);
+}
 
 /**
  * This method is used for transforming the text to the target type.
@@ -42,7 +42,7 @@ var gulpText = function(caseType) {
         isBuffer = file.isBuffer();
         if (isBuffer) {
             inputString = new String(file.contents);
-            result = transformText(caseType, inputString);
+            result = transformText(inputString);
 
             outBuffer = new Buffer(result);
             var aFile = new gutil.File(); // gulp expects a vinyl file
